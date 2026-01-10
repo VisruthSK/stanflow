71f7a36223020815d0e5517b536ecc9abecf82e3
VisruthSK <67435125+VisruthSK@users.noreply.github.com>
2026-01-10 13:49:36 -0800

    Upgrade packages in the action

--- tests/testthat/helper-interfaces-ci.R
+++ tests/testthat/helper-interfaces-ci.R
@@ -8,10 +8,7 @@ run_interface_setup <- function(interface, brms_backend = NULL) {
     brms_backend = brms_backend
   )
 
-  expect_true(
-    requireNamespace(interface, quietly = TRUE),
-    info = paste("Missing package:", interface)
-  )
   expect_false(is.null(getOption("mc.cores")))
+  expect_true(interface %in% loadedNamespaces())
   expect_true(paste0("package:", interface) %in% search())
 }
