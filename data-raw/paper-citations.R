d69b4afb0b5e19fcbd7c91720723a5449dd15b85
VisruthSK <67435125+VisruthSK@users.noreply.github.com>
2026-01-23 00:01:58 -0800

    Generated some bayesplot citations

--- data-raw/paper-citations.R
+++ data-raw/paper-citations.R
@@ -1,56 +1,127 @@
-.stan_citation_pkgs$brms <- c(
-  bibentry(
-    bibtype = "Article",
-    title = "{brms}: An {R} Package for {Bayesian} Multilevel Models Using {Stan}",
-    author = person(given = "Paul-Christian", family = "B\\u00fcrkner"),
-    journal = "Journal of Statistical Software",
-    year = "2017",
-    volume = "80",
-    number = "1",
-    pages = "1--28",
-    doi = "10.18637/jss.v080.i01",
-    textVersion = paste(
-      "Paul-Christian B\\u00fcrkner (2017).",
-      "brms: An R Package for Bayesian Multilevel Models Using Stan.",
-      "Journal of Statistical Software, 80(1), 1-28.",
-      "doi:10.18637/jss.v080.i01"
-    ),
-    encoding = "UTF-8"
+BDA <- bibentry(
+  bibtype = "Book",
+  key = "BDA",
+  title = "Bayesian Data Analysis",
+  author = c(
+    person("Andrew", "Gelman"),
+    person("John", "Carlin"),
+    person("Hal", "Stern"),
+    person("David", "Dunson"),
+    person("Aki", "Vehtari"),
+    person("Donald", "Rubin")
   ),
-  bibentry(
-    bibtype = "Article",
-    title = "Advanced {Bayesian} Multilevel Modeling with the {R} Package {brms}",
-    author = person(given = "Paul-Christian", family = "B\\u00fcrkner"),
-    journal = "The R Journal",
-    year = "2018",
-    volume = "10",
-    number = "1",
-    pages = "395--411",
-    doi = "10.32614/RJ-2018-017",
-    textVersion = paste(
-      "Paul-Christian B\\u00fcrkner (2018).",
-      "Advanced Bayesian Multilevel Modeling with the R Package brms.",
-      "The R Journal, 10(1), 395-411.",
-      "doi:10.32614/RJ-2018-017"
-    ),
-    encoding = "UTF-8"
+  year = "2013",
+  publisher = "Chapman & Hall/CRC",
+  address = "London",
+  edition = "3"
+)
+
+kaplan <- bibentry(
+  bibtype = "Article",
+  key = "kaplan",
+  title = "Nonparametric estimation from incomplete observations",
+  author = c(person("Edward", "Kaplan"), person("Paul", "Meier")),
+  journal = "Journal of the American Statistical Association",
+  year = "1958",
+  volume = "53",
+  number = "282",
+  pages = "457--481",
+  doi = "10.1080/01621459.1958.10501452"
+)
+
+betancourt2017 <- bibentry(
+  bibtype = "Misc",
+  key = "betancourt-2017-hmc-intro",
+  title = "A conceptual introduction to Hamiltonian Monte Carlo",
+  author = person("Michael", "Betancourt"),
+  year = "2017",
+  note = "arXiv preprint: https://arxiv.org/abs/1701.02434"
+)
+
+betancourt_girolami2013 <- bibentry(
+  bibtype = "Misc",
+  key = "betancourt-girolami-2013-hmc-hierarchical",
+  title = "Hamiltonian Monte Carlo for hierarchical models",
+  author = c(person("Michael", "Betancourt"), person("Mark", "Girolami")),
+  year = "2013",
+  note = "arXiv preprint: https://arxiv.org/abs/1312.0906"
+)
+
+vehtari2019_rhat <- bibentry(
+  bibtype = "Misc",
+  key = "vehtari-2019-rhat",
+  title = "Rank-normalization, folding, and localization: An improved R-hat for assessing convergence of MCMC",
+  author = c(
+    person("Aki", "Vehtari"),
+    person("Andrew", "Gelman"),
+    person("Daniel", "Simpson"),
+    person("Bob", "Carpenter"),
+    person(given = "Paul-Christian", family = "B\\u00fcrkner")
   ),
-  bibentry(
-    bibtype = "Article",
-    title = "Bayesian Item Response Modeling in {R} with {brms} and {Stan}",
-    author = person(given = "Paul-Christian", family = "B\\u00fcrkner"),
-    journal = "Journal of Statistical Software",
-    year = "2021",
-    volume = "100",
-    number = "5",
-    pages = "1--54",
-    doi = "10.18637/jss.v100.i05",
-    textVersion = paste(
-      "Paul-Christian B\\u00fcrkner (2021).",
-      "Bayesian Item Response Modeling in R with brms and Stan.",
-      "Journal of Statistical Software, 100(5), 1-54.",
-      "doi:10.18637/jss.v100.i05"
-    ),
-    encoding = "UTF-8"
-  )
+  year = "2019",
+  note = "arXiv preprint: https://arxiv.org/abs/1903.08008"
+)
+
+vehtari2017_loo <- bibentry(
+  bibtype = "Article",
+  key = "vehtari-2017-loo",
+  title = "Practical Bayesian model evaluation using leave-one-out cross-validation and WAIC",
+  author = c(
+    person("Aki", "Vehtari"),
+    person("Andrew", "Gelman"),
+    person("Jonah", "Gabry")
+  ),
+  journal = "Statistics and Computing",
+  year = "2017",
+  volume = "27",
+  number = "5",
+  pages = "1413--1432",
+  doi = "10.1007/s11222-016-9696-4",
+  note = "arXiv preprint: https://arxiv.org/abs/1507.04544"
+)
+
+hoffman2014_nuts <- bibentry(
+  bibtype = "Article",
+  key = "hoffman-2014-nuts",
+  title = "The No-U-Turn Sampler: adaptively setting path lengths in Hamiltonian Monte Carlo",
+  author = c(person("Matthew", "Hoffman"), person("Andrew", "Gelman")),
+  journal = "Journal of Machine Learning Research",
+  year = "2014",
+  volume = "15",
+  pages = "1593--1623"
+)
+
+stan_users_guide <- bibentry(
+  bibtype = "Manual",
+  key = "stan",
+  title = "Stan Modeling Language Users Guide and Reference Manual",
+  author = person("Stan Development Team"),
+  year = "2026",
+  url = "https://mc-stan.org/users/documentation/"
+)
+
+sailynoja2021 <- bibentry(
+  bibtype = "Misc",
+  key = "sailynoja-2021-uniformity",
+  title = "Graphical Test for Discrete Uniformity and its Applications in Goodness of Fit Evaluation and Multiple Sample Comparison",
+  author = c(
+    person("Timo", "Sailynoja"),
+    person(given = "Paul-Christian", family = "B\\u00fcrkner"),
+    person("Aki", "Vehtari")
+  ),
+  year = "2021",
+  note = "arXiv preprint: https://arxiv.org/abs/2103.10522"
+)
+
+kleiber2016_rootogram <- bibentry(
+  bibtype = "Article",
+  key = "kleiber-2016-rootogram",
+  title = "Visualizing count data regressions using rootograms",
+  author = c(person("Christian", "Kleiber"), person("Achim", "Zeileis")),
+  journal = "The American Statistician",
+  year = "2016",
+  volume = "70",
+  number = "3",
+  pages = "296--303",
+  note = "arXiv preprint: https://arxiv.org/abs/1605.01311"
 )
--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -17,6 +17,62 @@ message(
 )
 
 .stan_citation_pkgs <- new.env(parent = emptyenv())
+.stan_citation_pkgs$brms <- c(
+  bibentry(
+    bibtype = "Article",
+    title = "{brms}: An {R} Package for {Bayesian} Multilevel Models Using {Stan}",
+    author = person(given = "Paul-Christian", family = "B\\u00fcrkner"),
+    journal = "Journal of Statistical Software",
+    year = "2017",
+    volume = "80",
+    number = "1",
+    pages = "1--28",
+    doi = "10.18637/jss.v080.i01",
+    textVersion = paste(
+      "Paul-Christian B\\u00fcrkner (2017).",
+      "brms: An R Package for Bayesian Multilevel Models Using Stan.",
+      "Journal of Statistical Software, 80(1), 1-28.",
+      "doi:10.18637/jss.v080.i01"
+    ),
+    encoding = "UTF-8"
+  ),
+  bibentry(
+    bibtype = "Article",
+    title = "Advanced {Bayesian} Multilevel Modeling with the {R} Package {brms}",
+    author = person(given = "Paul-Christian", family = "B\\u00fcrkner"),
+    journal = "The R Journal",
+    year = "2018",
+    volume = "10",
+    number = "1",
+    pages = "395--411",
+    doi = "10.32614/RJ-2018-017",
+    textVersion = paste(
+      "Paul-Christian B\\u00fcrkner (2018).",
+      "Advanced Bayesian Multilevel Modeling with the R Package brms.",
+      "The R Journal, 10(1), 395-411.",
+      "doi:10.32614/RJ-2018-017"
+    ),
+    encoding = "UTF-8"
+  ),
+  bibentry(
+    bibtype = "Article",
+    title = "Bayesian Item Response Modeling in {R} with {brms} and {Stan}",
+    author = person(given = "Paul-Christian", family = "B\\u00fcrkner"),
+    journal = "Journal of Statistical Software",
+    year = "2021",
+    volume = "100",
+    number = "5",
+    pages = "1--54",
+    doi = "10.18637/jss.v100.i05",
+    textVersion = paste(
+      "Paul-Christian B\\u00fcrkner (2021).",
+      "Bayesian Item Response Modeling in R with brms and Stan.",
+      "Journal of Statistical Software, 100(5), 1-54.",
+      "doi:10.18637/jss.v100.i05"
+    ),
+    encoding = "UTF-8"
+  )
+)
 
 missing <- .stan_pkgs[
   !vapply(.stan_pkgs, requireNamespace, logical(1), quietly = TRUE)
@@ -68,6 +124,7 @@ if (length(missing) > 0) {
 )
 
 source("data-raw/paper-citations.R")
+source("data-raw/bayesplot-citations.R")
 
 # Helper to determine origin
 get_origin <- function(pkg, name) {
@@ -131,6 +188,4 @@ save(
   compress = "xz"
 )
 
-message(
-  "Export list, index, origin map, and stdlib functions saved to R/sysdata.rda"
-)
+message("Saved sysdata.rda")
